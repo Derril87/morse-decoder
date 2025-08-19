3c0a6e29364e4c63d5deb163f0744064de520ec7
Derril87 <yaroslav.balyuk@gmail.com>
2025-08-19 19:27:39 -0400

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,34 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  const code = expr.match(/.{1,10}/g);
+  const arr = [];
+  let res = '';
+
+  for (let i = 0; i < code.length; i += 1) {
+    const el = code[i];
+    if (el === '**********') arr.push(' ');
+    else arr.push(el.match(/.{1,2}/g));
+  }
+
+  const symbols = arr.map((el) => {
+    let string = '';
+    if (el === ' ') return ' ';
+    el.forEach((element) => {
+      if (element === '10') string += '.';
+      if (element === '11') string += '-';
+    });
+    return string;
+  });
+
+  symbols.forEach((el) => {
+    if (el in MORSE_TABLE) {
+      res += MORSE_TABLE[el];
+    } else if (el === ' ') {
+      res += ' ';
+    }
+  });
+  return res;
 };
